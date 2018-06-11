d2566c47de397b4daa375887bd6b117492a7c4a5
miguel <miguel@CopperMind>
2018-06-11 02:23:48 +0200

    Add the JPA to the pom and convert Book in an entity

--- src/main/java/com/example/demo/book/Book.java
+++ src/main/java/com/example/demo/book/Book.java
@@ -1,7 +1,12 @@
 package com.example.demo.book;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Book {
 
+	@Id
 	private String id;
 	private String name;
 	private String description;
